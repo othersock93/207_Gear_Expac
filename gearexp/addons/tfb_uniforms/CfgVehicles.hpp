4678e50fc9f0ff0974c09d2aa6d092f7facc3f58
othersock93 <othersock93@gmail.com>
2026-07-09 16:45:25 +1000

    Update author attribution

--- gearexp/addons/tfb_uniforms/CfgVehicles.hpp
+++ gearexp/addons/tfb_uniforms/CfgVehicles.hpp
@@ -5,7 +5,7 @@ class tfb_g3_base_v: B_Soldier_base_F
 	scope = 0;
 	camouflage = 1;
 	scopeArsenal = 0;
-	author="207 Community Author";
+	author="Mac n Socks";
 	model = "tfb_uniforms\mesh\tfb_g3_g.p3d";
 	picture = "\tfb_uniforms\ui\g3_mc_ca.paa";
 	uniformClass = "tfb_g3_mc_g_w";
@@ -19,7 +19,7 @@ class tfb_g3_mc_g_v: tfb_g3_base_v
 	scope = 1;
 	scopeArsenal = 2;
 	camouflage = 1;
-	author="207 Community Author";
+	author="Mac n Socks";
 	model = "\tfb_uniforms\mesh\tfb_g3_g.p3d";
 	uniformClass = "tfb_g3_mc_g_w";
 	picture = "\tfb_uniforms\ui\g3_mc_ca.paa";
