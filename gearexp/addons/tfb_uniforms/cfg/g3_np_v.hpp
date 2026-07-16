8c6555f362d5ac127d77f7f4f2c7dd10abe1b7e7
Socks <othersock93@gmail.com>
2026-07-17 08:31:00 +1000

    Add files via upload

--- gearexp/addons/tfb_uniforms/cfg/g3_np_v.hpp
+++ gearexp/addons/tfb_uniforms/cfg/g3_np_v.hpp
@@ -3,7 +3,7 @@ class 207_g3_mc_np_g_v: 207_g3_base_v
 {
 	scope = 1;
 	scopeArsenal = 2;
-	model = "tfb_uniforms\mesh\207_g3_g.p3d";
+	model = "tfb_uniforms\mesh\tfb_g3_g.p3d";
 	uniformClass = "207_g3_mc_g_w";
 	picture = "\tfb_uniforms\ui\g3_mc_ca.paa";
 	hiddenSelections[] = 
@@ -33,7 +33,7 @@ class 207_g3_mc_r1_np_g_v: 207_g3_base_v
 {
 	scope = 1;
 	scopeArsenal = 2;
-	model = "\tfb_uniforms\mesh\207_g3_r1_g.p3d";
+	model = "\tfb_uniforms\mesh\tfb_g3_r1_g.p3d";
 	uniformClass = "207_g3_mc_r1_g_w";
 	picture = "\tfb_uniforms\ui\g3_mc_ca.paa";
 	hiddenSelections[] = 
@@ -63,7 +63,7 @@ class 207_g3_mc_r2_np_g_v: 207_g3_base_v
 {
 	scope = 1;
 	scopeArsenal = 2;
-	model = "\tfb_uniforms\mesh\207_g3_r2_g.p3d";
+	model = "\tfb_uniforms\mesh\tfb_g3_r2_g.p3d";
 	uniformClass = "207_g3_mc_r2_g_w";
 	picture = "\tfb_uniforms\ui\uni1_ca.paa";
 	hiddenSelections[] = 
@@ -94,7 +94,7 @@ class 207_g3_mc_np_v: 207_g3_base_v
 {
 	scope = 1;
 	scopeArsenal = 2;
-	model = "\tfb_uniforms\mesh\207_g3.p3d";
+	model = "\tfb_uniforms\mesh\tfb_g3.p3d";
 	uniformClass = "207_g3_mc_np_w";
 	picture = "\tfb_uniforms\ui\g3_mc_ca.paa";
 	hiddenSelections[] = 
@@ -124,7 +124,7 @@ class 207_g3_mc_r1_np_v: 207_g3_base_v
 {
 	scope = 1;
 	scopeArsenal = 2;
-	model = "\tfb_uniforms\mesh\207_g3_r1.p3d";
+	model = "\tfb_uniforms\mesh\tfb_g3_r1.p3d";
 	uniformClass = "207_g3_mc_r1_np_w";
 	picture = "\tfb_uniforms\ui\g3_mc_ca.paa";
 	hiddenSelections[] = 
@@ -154,7 +154,7 @@ class 207_g3_mc_r2_np_v: 207_g3_base_v
 {
 	scope = 1;
 	scopeArsenal = 2;
-	model = "\tfb_uniforms\mesh\207_g3_r2.p3d";
+	model = "\tfb_uniforms\mesh\tfb_g3_r2.p3d";
 	uniformClass = "207_g3_mc_r2_np_w";
 	picture = "\tfb_uniforms\ui\uni1_ca.paa";
 	hiddenSelections[] = 
