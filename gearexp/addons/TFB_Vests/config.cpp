99ce4e1df90e96042a8848cb261614e6d59f70f7
Socks <othersock93@gmail.com>
2026-07-17 08:24:19 +1000

    Add files via upload

--- gearexp/addons/TFB_Vests/config.cpp
+++ gearexp/addons/TFB_Vests/config.cpp
@@ -5,16 +5,16 @@ class CfgPatches
 		units[]={};
 		weapons[]=
 		{
-			"TFB_JPC_Assaulter_13_152A",
-			"TFB_JPC_Assaulter_13_152A_NB",
-			"TFB_JPC_Assaulter_10_148",
-			"TFB_JPC_Assaulter_10_148_NB",
-			"TFB_JPC_Comms_2_Dual_2",
-			"TFB_JPC_Comms_2_Dual_2_NB",
-			"TFB_JPC_Weapons_2_148",
-			"TFB_JPC_Weapons_2_148_NB",
-			"TFB_JPC_Weapons_6_MPU5",
-			"TFB_JPC_Weapons_6_MPU5_NB"
+			"AMC_JPC_Assaulter_13_152A",
+			"AMC_JPC_Assaulter_13_152A_NB",
+			"AMC_JPC_Assaulter_10_148",
+			"AMC_JPC_Assaulter_10_148_NB",
+			"AMC_JPC_Comms_2_Dual_2",
+			"AMC_JPC_Comms_2_Dual_2_NB",
+			"AMC_JPC_Weapons_2_148",
+			"AMC_JPC_Weapons_2_148_NB",
+			"AMC_JPC_Weapons_6_MPU5",
+			"AMC_JPC_Weapons_6_MPU5_NB"
 		};
 		requiredVersion=0.1;
 		requiredAddons[]=
@@ -27,7 +27,7 @@ class CfgPatches
 
 class XtdGearModels {
     class CfgWeapons {
-        class tfb_jpc_assaulter
+        class amc_jpc_assaulter
         {
             options[] = 
 			{
@@ -56,7 +56,7 @@ class XtdGearModels {
 				}; 
             };
         };
-        class tfb_jpc_comms
+        class amc_jpc_comms
         {
             options[] = 
 			{
@@ -84,7 +84,7 @@ class XtdGearModels {
 				}; 
             };
         };
-        class tfb_jpc_weapons
+        class amc_jpc_weapons
         {
             options[] = 
 			{
@@ -118,62 +118,62 @@ class XtdGearModels {
 
 class XtdGearInfos {
     class CfgWeapons {
-        class TFB_JPC_Assaulter_13_152A {
-			model = "tfb_jpc_assaulter"
+        class AMC_JPC_Assaulter_13_152A {
+			model = "amc_jpc_assaulter"
           CONFIGURATION = "ASSAULTER";
           SETUP = "B";
 		  BELT = "ON";
         };
-        class TFB_JPC_Assaulter_13_152A_NB {
-			model = "tfb_jpc_assaulter"
+        class AMC_JPC_Assaulter_13_152A_NB {
+			model = "amc_jpc_assaulter"
           CONFIGURATION = "ASSAULTER";
           SETUP = "B";
 		  BELT = "OFF";
         };
-		class TFB_JPC_Assaulter_10_148 {
-			model = "tfb_jpc_assaulter"
+		class AMC_JPC_Assaulter_10_148 {
+			model = "amc_jpc_assaulter"
           CONFIGURATION = "ASSAULTER";
           SETUP = "A";
 		  BELT = "ON";
         };
-		class TFB_JPC_Assaulter_10_148_NB {
-			model = "tfb_jpc_assaulter"
+		class AMC_JPC_Assaulter_10_148_NB {
+			model = "amc_jpc_assaulter"
           CONFIGURATION = "ASSAULTER";
           SETUP = "A";
 		  BELT = "OFF";
         };
-		class TFB_JPC_Comms_2_Dual_2 {
-			model = "tfb_jpc_comms"
+		class AMC_JPC_Comms_2_Dual_2 {
+			model = "amc_jpc_comms"
           CONFIGURATION = "COMMS";
           SETUP = "A";
 		  BELT = "ON";
         };
-		class TFB_JPC_Comms_2_Dual_2_NB {
-			model = "tfb_jpc_comms"
+		class AMC_JPC_Comms_2_Dual_2_NB {
+			model = "amc_jpc_comms"
           CONFIGURATION = "COMMS";
           SETUP = "A";
 		  BELT = "OFF";
         };
-		class TFB_JPC_Weapons_2_148 {
-			model = "tfb_jpc_weapons"
+		class AMC_JPC_Weapons_2_148 {
+			model = "amc_jpc_weapons"
           CONFIGURATION = "WEAPONS";
           SETUP = "A";
 		  BELT = "ON";
         };
-		class TFB_JPC_Weapons_2_148_NB {
-			model = "tfb_jpc_weapons"
+		class AMC_JPC_Weapons_2_148_NB {
+			model = "amc_jpc_weapons"
           CONFIGURATION = "WEAPONS";
           SETUP = "A";
 		  BELT = "OFF";
         };
-		class TFB_JPC_Weapons_6_MPU5 {
-		  model = "tfb_jpc_weapons"
+		class AMC_JPC_Weapons_6_MPU5 {
+		  model = "amc_jpc_weapons"
           CONFIGURATION = "WEAPONS";
           SETUP = "B";
 		  BELT = "ON";
         };
-		class TFB_JPC_Weapons_6_MPU5_NB {
-		  model = "tfb_jpc_weapons"
+		class AMC_JPC_Weapons_6_MPU5_NB {
+		  model = "amc_jpc_weapons"
           CONFIGURATION = "WEAPONS";
           SETUP = "B";
 		  BELT = "OFF";
@@ -186,7 +186,7 @@ class cfgWeapons
 	class Vest_Camo_Base;
 	class VestItem;
 	class ItemCore;
-	class TFB_JPC_Assaulter_1_148: ItemCore
+	class AMC_JPC_Assaulter_1_148: ItemCore
 	{
 		author="Mac n Socks";
 		scope=0;
@@ -228,62 +228,62 @@ class cfgWeapons
 			};
 		};
 	};
-	class TFB_JPC_Assaulter_10_148: TFB_JPC_Assaulter_1_148
+	class AMC_JPC_Assaulter_10_148: AMC_JPC_Assaulter_1_148
 	{
 		scope=2;
 		picture="\TFB_Vests\ui\assves_ca.paa";
-		displayname="JPC Vest Assaulter 1";
+		displayname="JPC Vest Assaulter 1 (AMC)";
 		model="TFB_Vests\JPC\JPC_Assaulter_10_148.p3d";
 		class ItemInfo: ItemInfo
 		{
 			uniformModel="TFB_Vests\JPC\JPC_Assaulter_10_148.p3d";
 		};
 	};
-	class TFB_JPC_Assaulter_13_152A: TFB_JPC_Assaulter_1_148
+	class AMC_JPC_Assaulter_13_152A: AMC_JPC_Assaulter_1_148
 	{
 		scope=2;
 		picture="\TFB_Vests\ui\assves2_ca.paa";
-		displayname="JPC Vest Assaulter 2";
+		displayname="JPC Vest Assaulter 2 (AMC)";
 		model="TFB_Vests\JPC\JPC_Assaulter_13_152A.p3d";
 		class ItemInfo: ItemInfo
 		{
 			uniformModel="TFB_Vests\JPC\JPC_Assaulter_13_152A.p3d";
 		};
 	};
-	class TFB_JPC_Comms_2_Dual_2: TFB_JPC_Assaulter_1_148
+	class AMC_JPC_Comms_2_Dual_2: AMC_JPC_Assaulter_1_148
 	{
 		scope=2;
 		picture="\TFB_Vests\ui\comves_ca.paa";
-		displayname="JPC Vest Comms 1";
+		displayname="JPC Vest Comms 1 (AMC)";
 		model="TFB_Vests\JPC\JPC_Comms_2_Dual_2.p3d";
 		class ItemInfo: ItemInfo
 		{
 			uniformModel="TFB_Vests\JPC\JPC_Comms_2_Dual_2.p3d";
 		};
 	};
-	class TFB_JPC_Weapons_2_148: TFB_JPC_Assaulter_1_148
+	class AMC_JPC_Weapons_2_148: AMC_JPC_Assaulter_1_148
 	{
 		scope=2;
 		picture="\TFB_Vests\ui\wepves_ca.paa";
-		displayname="JPC Vest Weapons 1";
+		displayname="JPC Vest Weapons 1 (AMC)";
 		model="TFB_Vests\JPC\JPC_Weapons_2_148.p3d";
 		class ItemInfo: ItemInfo
 		{
 			uniformModel="TFB_Vests\JPC\JPC_Weapons_2_148.p3d";
 		};
 	};
-	class TFB_JPC_Weapons_6_MPU5: TFB_JPC_Assaulter_1_148
+	class AMC_JPC_Weapons_6_MPU5: AMC_JPC_Assaulter_1_148
 	{
 		scope=2;
 		picture="\TFB_Vests\ui\wepves2_ca.paa";
-		displayname="JPC Vest Weapons 2";
+		displayname="JPC Vest Weapons 2 (AMC)";
 		model="TFB_Vests\JPC\JPC_Weapons_6_MPU5.p3d";
 		class ItemInfo: ItemInfo
 		{
 			uniformModel="TFB_Vests\JPC\JPC_Weapons_6_MPU5.p3d";
 		};
 	};
-	class TFB_JPC_Assaulter_1_148_NB: ItemCore
+	class AMC_JPC_Assaulter_1_148_NB: ItemCore
 	{
 		author="Mac n Socks";
 		scope=0;
@@ -325,55 +325,55 @@ class cfgWeapons
 			};
 		};
 	};
-	class TFB_JPC_Assaulter_10_148_NB: TFB_JPC_Assaulter_1_148_NB
+	class AMC_JPC_Assaulter_10_148_NB: AMC_JPC_Assaulter_1_148_NB
 	{
 		scope=2;
 		picture="\TFB_Vests\ui\assves_ca.paa";
-		displayname="JPC Vest Assaulter 1, No Belt";
+		displayname="JPC Vest Assaulter 1, No Belt (AMC)";
 		model="TFB_Vests\JPC\JPC_Assaulter_10_148_NB.p3d";
 		class ItemInfo: ItemInfo
 		{
 			uniformModel="TFB_Vests\JPC\JPC_Assaulter_10_148_NB.p3d";
 		};
 	};
-	class TFB_JPC_Assaulter_13_152A_NB: TFB_JPC_Assaulter_1_148_NB
+	class AMC_JPC_Assaulter_13_152A_NB: AMC_JPC_Assaulter_1_148_NB
 	{
 		scope=2;
 		picture="\TFB_Vests\ui\assves2_ca.paa";
-		displayname="JPC Vest Assaulter 2, No Belt";
+		displayname="JPC Vest Assaulter 2, No Belt (AMC)";
 		model="TFB_Vests\JPC\JPC_Assaulter_13_152A_NB.p3d";
 		class ItemInfo: ItemInfo
 		{
 			uniformModel="TFB_Vests\JPC\JPC_Assaulter_13_152A_NB.p3d";
 		};
 	};
-	class TFB_JPC_Comms_2_Dual_2_NB: TFB_JPC_Assaulter_1_148_NB
+	class AMC_JPC_Comms_2_Dual_2_NB: AMC_JPC_Assaulter_1_148_NB
 	{
 		scope=2;
 		picture="\TFB_Vests\ui\comves_ca.paa";
-		displayname="JPC Vest Comms 1, No Belt";
+		displayname="JPC Vest Comms 1, No Belt (AMC)";
 		model="TFB_Vests\JPC\JPC_Comms_2_Dual_2_NB.p3d";
 		class ItemInfo: ItemInfo
 		{
 			uniformModel="TFB_Vests\JPC\JPC_Comms_2_Dual_2_NB.p3d";
 		};
 	};
-	class TFB_JPC_Weapons_2_148_NB: TFB_JPC_Assaulter_1_148_NB
+	class AMC_JPC_Weapons_2_148_NB: AMC_JPC_Assaulter_1_148_NB
 	{
 		scope=2;
 		picture="\TFB_Vests\ui\wepves_ca.paa";
-		displayname="JPC Vest Weapons 1, No Belt";
+		displayname="JPC Vest Weapons 1, No Belt (AMC)";
 		model="TFB_Vests\JPC\JPC_Weapons_2_148_NB.p3d";
 		class ItemInfo: ItemInfo
 		{
 			uniformModel="TFB_Vests\JPC\JPC_Weapons_2_148_NB.p3d";
 		};
 	};
-	class TFB_JPC_Weapons_6_MPU5_NB: TFB_JPC_Assaulter_1_148_NB
+	class AMC_JPC_Weapons_6_MPU5_NB: AMC_JPC_Assaulter_1_148_NB
 	{
 		scope=2;
 		picture="\TFB_Vests\ui\wepves2_ca.paa";
-		displayname="JPC Vest Weapons 2, No Belt";
+		displayname="JPC Vest Weapons 2, No Belt (AMC)";
 		model="TFB_Vests\JPC\JPC_Weapons_6_MPU5_NB.p3d";
 		class ItemInfo: ItemInfo
 		{
